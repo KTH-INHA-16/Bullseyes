4d30202390e44ac2ecd71de01618b37dcfa2dc5a
KTH-INHA-16 <kim1120318@naver.com>
2022-05-17 15:45:00 +0900

    add: project

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by 김태훈 on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
